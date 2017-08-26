212d738f7d1aa90db564be49c67553831b032e98
Sierra Gregg <sierragregg@gmail.com>
2017-08-26 14:30:47 -0500

    Reworked logic based on https://bl.ocks.org/mbostock/34f08d5e11952a80609169b7917d4172

--- js/bar-chart.js
+++ js/bar-chart.js
@@ -1,177 +1,166 @@
-function zoomBarChart(svg) {
-
-  /****** Declare Local Variables ******/
-
-  // Universal, apply to both chart and selector
-  var _chartObj = {},
-      _svg = svg,
-      _svgWidth = +svg.attr('width'), _svgHeight = +svg.attr('height'),
-      _data = [];
-
-  // Chart only variables
-  var _chart,
-      _chartWrapper,
-      _chartMargins = {top: 10, right: 10, bottom: 140, left: 40},
-      _chartHeight = _svgHeight - _chartMargins.top - _chartMargins.bottom,
-      _chartX, _chartY,
-      _chartXAxis, _chartYAxis;
-  
-  // Selector only variables
-  var _selector,
-      _selectorWrapper,
-      _selectorMargins = {top: 460, right: 10, bottom: 30, left: 40},
-      _selectorHeight = _svgHeight - _selectorMargins.top - _selectorMargins.bottom,
-      _selectorX, _selectorY, 
-      _selectorXAxis, _selectorYAxis;
-
-  // One last Universal Variable
-  var _width = _svgWidth - _chartMargins.left - _chartMargins.right;
-
-  _chartObj.chartX = function (x) {
-    if (!arguments.length) return _chartX;
-    console.log(x)
-    _chartX = x;
-    return _chartObj;
-  };
-
-  _chartObj.chartY = function (y) {
-    if (!arguments.length) return _chartY;
-    console.log(y)
-    _chartY = y;
-    return _chartObj;
-  };
-
-  _chartObj.setData = function (d) {
-    _data = d;
-    return _chartObj;
-  };
-
-  /****** Draw the Chart ******/
-
-  _chartObj.render = function () {
-    _svg = d3.select('#chart')
-              .attr("height", _svgHeight)
-              .attr("width", _svgWidth);
-
-    _chart = svg.append("g")
-                    .attr("class", "chart")
-                    .attr("transform", "translate(" + 
-                        _chartMargins.left + "," + _chartMargins.top + ")");
-    renderChartAxes(svg);
-    renderSelectorAxes(svg);
-
-    _selector = svg.append('g')
-                    .attr('class', 'selector')
-                    .attr('transform', 'translate(' +
-                        0 + ',' + 0 + ')');
-    
-    
-    
-    console.log(_data);
-    renderBars();
-    // defineBodyClip(_chart);
-
-    // renderBody(_svg);
-  };
-
-  function renderChartAxes(chart) {
-    var axesG = svg.select("g")
-                    .attr("class", "axes");
-
-    var xAxis = d3.axisBottom().scale(_chartX.range([0, _width]));
-    var yAxis = d3.axisLeft().scale(_chartY.range([_chartHeight, 0]));
-
-    axesG.append("g")
-            .attr("class", "axis")
-            .attr("transform", function () {
-                return "translate(" + _chartMargins.left + "," + (_chartMargins.top + _chartY(0.0)) + ")";
-            })
-            .call(xAxis);
-
-    axesG.append("g")
-            .attr("class", "axis")
-            .attr("transform", function () {
-                return "translate(" + _chartMargins.left + "," + _chartMargins.top + ")";
-            })
-            .call(yAxis);
-  }
-
-  function renderSelectorAxes(selector) {
-    var axesG = svg.select('.axes');
-
-    var xAxis = d3.axisBottom().scale(_chartX.range([0, _width]));
-    var yAxis = d3.axisLeft().scale(_chartY.range([_selectorHeight, 0]));
-
-    axesG.append("g")
-            .attr("class", "axis")
-            .attr("transform", function () {
-                return "translate(" + _selectorMargins.left + "," + (_selectorMargins.top + _selectorHeight)  + ")";
-            })
-            .call(xAxis);
-  }
- 
-  function renderBars() {
-    var groupByYear = d3.nest()
-        .key(function(d) { return d.date.getFullYear();})
-        .rollup(function(v) { return d3.mean(v, function(d) { return d.temp; }); })
-        .entries(_data);
-    console.log(groupByYear)
-    var padding = 2; // <-A
-
-    var bars = _chart.selectAll('rect.bar')
-            .data(groupByYear);
-    console.log(_chartY(0.00))
-    console.log()
-    bars.enter()
-            .append('rect')
-        .merge(bars)
-            .attr('class', 'bar')
-        .transition()
-            .attr('x', function (d) { 
-                return _chartX(new Date(d.key, 0, 1));
-            })
-            .attr('y', function (d) {
-                return d.value < 0 ? (_chartHeight / 2 + _chartY(0.0)) : _chartY(d.value);
-            })
-            .attr('height', function (d) { 
-                return (_chartMargins.top + _chartY(0.0)) - _chartY(d.value); 
-            })
-            .attr('width', function(d){
-                return Math.floor(_width / groupByYear.length) - padding;
-            });
-  }
-
-  return _chartObj; 
-}
-var parseDate = d3.timeParse("%b %Y");
-
-function processData(rawData) {
-  var months = ['Apr','Aug','Dec','Feb','Jan','Jul','Jun','Mar',
-                'May','Nov','Oct','Sep'];
-  var newData = [];
-
-  for(var i = 0; i < rawData.length; i++) {
-    var year = rawData[i].Year;
-    for(var j = 0; j < months.length; j++) {
-      newData.push({
-        date: parseDate(months[j] + ' ' + year),
-        temp: +rawData[i][months[j]],
-      });
-    }
-  }
-
-  return newData;
-}
-
 function init() {
-  d3.csv('data/GLB.Ts.csv', function(result) {
-    var data = processData(result);
+    var svg = d3.select("svg"),
+        margin = {top: 20, right: 20, bottom: 110, left: 40},
+        margin2 = {top: 430, right: 20, bottom: 30, left: 40},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom,
+        height2 = +svg.attr("height") - margin2.top - margin2.bottom;
 
-    var chartArea = d3.select("#chart");
-    var chart = zoomBarChart(chartArea)
-                .chartX(d3.scaleTime().domain(d3.extent(data, function(d) { return d.date; })))
-                .chartY(d3.scaleLinear().domain(d3.extent(data, function(d) { return d.temp; })));
-    chart.setData(data);
-    chart.render();
-  });
+    var parseDate = d3.timeParse("%b %Y");
+
+    var x = d3.scaleTime().range([0, width]),
+        x2 = d3.scaleTime().range([0, width]),
+        y = d3.scaleLinear().range([height, 0]),
+        y2 = d3.scaleLinear().range([height2, 0]);
+
+    var xAxis = d3.axisBottom(x),
+        xAxis2 = d3.axisBottom(x2),
+        yAxis = d3.axisLeft(y);
+
+    var brush = d3.brushX()
+        .extent([[0, 0], [width, height2]])
+        .on("brush end", brushed);
+
+    var zoom = d3.zoom()
+        .scaleExtent([1, Infinity])
+        .translateExtent([[0, 0], [width, height]])
+        .extent([[0, 0], [width, height]])
+        .on("zoom", zoomed);
+
+    // var area = d3.area()
+    //     .curve(d3.curveMonotoneX)
+    //     .x(function(d) { return x(d.date); })
+    //     .y0(height)
+    //     .y1(function(d) { return y(d.temp); });
+    // console.log('area', area)
+    // var area2 = d3.area()
+    //     .curve(d3.curveMonotoneX)
+    //     .x(function(d) { return x2(d.date); })
+    //     .y0(height2)
+    //     .y1(function(d) { return y2(d.temp); });
+
+    var line = d3.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.temp); });
+
+    var line2 = d3.line()
+        .x(function(d) { return x2(d.date); })
+        .y(function(d) { return y2(d.temp); });
+
+    svg.append("defs").append("clipPath")
+        .attr("id", "clip")
+    .append("rect")
+        .attr("width", width)
+        .attr("height", height);
+
+    var focus = svg.append("g")
+        .attr("class", "focus")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var context = svg.append("g")
+        .attr("class", "context")
+        .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+    d3.csv("data/GLB.Ts.csv", type, function(error, data) {
+    if (error) throw error;
+        console.log(data)
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain(d3.extent(data, function(d) { return d.temp; }));
+    x2.domain(x.domain());
+    y2.domain(y.domain());
+
+    focus.append("path")
+        .datum(data)
+        .attr('class', 'line')
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
+
+    //   focus.append("path")
+    //       .datum(data)
+    //       .attr("class", "area")
+    //       .attr("d", area);
+
+    focus.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    focus.append("g")
+        .attr("class", "axis axis--y")
+        .call(yAxis);
+
+        context.append("path")
+        .datum(data)
+        .attr('class', 'line')
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line2);
+
+    //   context.append("path")
+    //       .datum(data)
+    //       .attr("class", "area")
+    //       .attr("d", area2);
+
+    context.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height2 + ")")
+        .call(xAxis2);
+
+    context.append("g")
+        .attr("class", "brush")
+        .call(brush)
+        .call(brush.move, x.range());
+
+    svg.append("rect")
+        .attr("class", "zoom")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .call(zoom);
+    });
+
+    function brushed() {
+    if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
+    console.log('d3.event.selection', d3.event.selection)
+    
+    var s = d3.event.selection || x2.range();
+    x.domain(s.map(x2.invert, x2));
+    focus.select(".line").attr("d", line);
+    focus.select(".axis--x").call(xAxis);
+    svg.select(".zoom").call(zoom.transform, d3.zoomIdentity
+        .scale(width / (s[1] - s[0]))
+        .translate(-s[0], 0));
+    }
+
+    function zoomed() {
+    if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
+    var t = d3.event.transform;
+    x.domain(t.rescaleX(x2).domain());
+    focus.select(".line").attr("d", line);
+    focus.select(".axis--x").call(xAxis);
+    context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
+    }
+
+    function type(inputObj) {
+        var months = ['Apr','Aug','Dec','Feb','Jan','Jul','Jun','Mar',
+                        'May','Nov','Oct','Sep'];
+        var outputObj = {};
+        var year = inputObj.Year;
+        for(var j = 0; j < months.length; j++) {
+            outputObj.date = parseDate(months[j] + ' ' + year);
+            outputObj.temp = +inputObj[months[j]];
+        }
+
+    //   d.date = parseDate(d.date);
+    //   d.price = +d.price;
+    //   return d;
+        return outputObj;
+    }
 }
