37cdffc8a4d5852f4a6e2d4275e226f00b6003ac
Sierra Gregg <sierragregg@gmail.com>
2017-08-26 21:49:44 -0500

    Legend,refractor,source links

--- src/js/bar-chart.js
+++ src/js/bar-chart.js
@@ -1,17 +1,17 @@
-function init() {
+function zoomBarChart() {
     /****** LOCAL VARIABLES ******/
 
     // Elements required to render the visualization
-    var svg = d3.select("svg"),
+    var svg = d3.select('svg'),
         _xChart, _yChart, _xChartAxis, _yChartAxis,
         _chart, _selector, _brush, _zoom;
     
     // Dimensions used to calculate element placement
     var chartMargin = {top: 20, right: 20, bottom: 120, left: 70},
         selectorMargin = {top: 420, right: 20, bottom: 40, left: 70},
-        drawWidth = +svg.attr("width") - chartMargin.left - chartMargin.right,
-        chartHeight = +svg.attr("height") - chartMargin.top - chartMargin.bottom,
-        selectorHeight = +svg.attr("height") - selectorMargin.top - selectorMargin.bottom;
+        drawWidth = +svg.attr('width') - chartMargin.left - chartMargin.right,
+        chartHeight = +svg.attr('height') - chartMargin.top - chartMargin.bottom,
+        selectorHeight = +svg.attr('height') - selectorMargin.top - selectorMargin.bottom;
 
     // Information being graphed
     var _rawData = [];
@@ -26,7 +26,7 @@ function init() {
 
     /****** READ AND RENDER DATA ******/
 
-    d3.csv("data/GLB.Ts.csv", clearAndFilter, function(error, data) {
+    d3.csv('data/GLB.Ts.csv', clearAndFilter, function(error, data) {
         if (error) {
             console.error(error);
         }
@@ -41,6 +41,8 @@ function init() {
     
         renderBars(_chart, _byYear, _xChart, _yChart, chartHeight, true);
         renderBars(_selector, _byYear, _xSelector, _ySelector, selectorHeight, false);
+
+        renderLegend();
     });
 
     /****** HELPER FUNCTIONS ******/
@@ -49,54 +51,54 @@ function init() {
     function renderBody() {
         // Add clip path so that bars do not render
         // outside chart area
-        svg.append("defs").append("clipPath")
-            .attr("id", "clip")
-            .append("rect")
-            .attr("width", drawWidth)
-            .attr("height", chartHeight);
+        svg.append('defs').append('clipPath')
+            .attr('id', 'clip')
+            .append('rect')
+            .attr('width', drawWidth)
+            .attr('height', chartHeight);
 
         // Append areas for the chart and selector to the
         // svg
-        _chart = svg.append("g")
-            .attr("class", "_chart")
-            .attr("transform", "translate(" + 
-                chartMargin.left + "," + chartMargin.top + ")");
+        _chart = svg.append('g')
+            .attr('class', '_chart')
+            .attr('transform', 'translate(' + 
+                chartMargin.left + ',' + chartMargin.top + ')');
 
-        _selector = svg.append("g")
-            .attr("class", "_selector")
-            .attr("transform", "translate(" + 
-                selectorMargin.left + "," + selectorMargin.top + ")");
+        _selector = svg.append('g')
+            .attr('class', '_selector')
+            .attr('transform', 'translate(' + 
+                selectorMargin.left + ',' + selectorMargin.top + ')');
 
     }
 
     // Render the bars
     function renderBars(element, data, x, y, eleHeight, clip) {
         console.log('width: Math.floor(' + drawWidth + '/' + data.length + ') - 1');
-        var b = element.selectAll("rect.bar")
+        var b = element.selectAll('rect.bar')
                 .data(data);
                 console.log(Math.floor(drawWidth / data.length))
         b.enter()
-            .append("rect")
+            .append('rect')
         .merge(b)
-            .attr("class", "bar")
+            .attr('class', 'bar')
             .attr('fill', function(d) {
                 return d.temp < 0 ? '#1ae9f0' : '#662112';
             })
-            .attr("x", function (d) { 
+            .attr('x', function (d) { 
                 return x(d.date);
             })
-            .attr("y", function (d) { 
+            .attr('y', function (d) { 
                 return isNaN(y(d.temp)) ? 0 : y(d.temp);
             })
-            .attr("height", function (d) { 
+            .attr('height', function (d) { 
                 return isNaN(y(d.temp)) ? 0 : eleHeight - y(d.temp); 
             })
-            .attr("width", function(d){
+            .attr('width', function(d){
                 return Math.floor(drawWidth / data.length) - 1;
             });
 
         if(clip) {
-            b.attr("clip-path", "url(#clip)");
+            b.attr('clip-path', 'url(#clip)');
         }
     }
 
@@ -127,39 +129,45 @@ function init() {
 
     function renderAxes() {
         // X-Axes
-        _chart.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + chartHeight + ")")
+        _chart.append('g')
+            .attr('class', 'axis axis--x')
+            .attr('transform', 'translate(0,' + chartHeight + ')')
             .call(_xChartAxis);
 
-        _selector.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + selectorHeight + ")")
+        _selector.append('g')
+            .attr('class', 'axis axis--x')
+            .attr('transform', 'translate(0,' + selectorHeight + ')')
             .call(_xSelectorAxis);
 
         // Y-axis
-        _chart.append("g")
-            .attr("class", "axis axis--y")
+        _chart.append('g')
+            .attr('class', 'axis axis--y')
             .call(_yChartAxis);
     
-        _selector.append("g")
-            .attr("class", "brush")
+        _selector.append('g')
+            .attr('class', 'brush')
             .call(_brush)
             .call(_brush.move, _xChart.range());
         
         // Axes Titles
         _chart.append('text')
-            .attr("text-anchor", "middle") 
-            .attr("transform", "translate("+  (-chartMargin.left/2) +","+(chartHeight/2)+")rotate(-90)")
+            .attr('text-anchor', 'middle') 
+            .attr('transform', 'translate('+  (-chartMargin.left/2) +','+(chartHeight/2)+')rotate(-90)')
             .text('Mean Temperatures in Celsius');
 
-        _selector.append("text")
-            .attr("text-anchor", "middle") 
-            .attr("transform", "translate("+  (drawWidth/2) +","+(selectorHeight + 40)+")")
-            .text("Date");
+        _selector.append('text')
+            .attr('text-anchor', 'middle') 
+            .attr('transform', 'translate('+  (drawWidth/2) +','+(selectorHeight + 40)+')')
+            .text('Date');
     }
     
+    // Add lines to create a border for the chart.
+    // Add a line bisecting the chart at 0, providing
+    // another visual que differentiating positive
+    // from negative numbers.
     function renderGuideLines() {
+
+        // Bisect the chart at zero
         _chart.append('line')
                 .attr('x1', 0)
                 .attr('y1', _yChart(0))
@@ -168,6 +176,15 @@ function init() {
                 .attr('stroke-width', 1)
                 .attr('stroke', 'rgba(15, 15, 15, 0.61)');
         
+        _selector.append('line')
+                .attr('x1', 0)
+                .attr('y1', _ySelector(0))
+                .attr('x2', drawWidth)
+                .attr('y2', _ySelector(0))
+                .attr('stroke-width', 1)
+                .attr('stroke', 'rgba(15, 15, 15, 0.61)');
+        
+        // Create a border for the chart
         _chart.append('line')
                 .attr('x1', 0)
                 .attr('y1', 0)
@@ -183,32 +200,81 @@ function init() {
                 .attr('y2', chartHeight)
                 .attr('stroke-width', 1)
                 .attr('stroke', 'rgba(15, 15, 15, 0.61)');
+    }
 
-        _selector.append('line')
-                .attr('x1', 0)
-                .attr('y1', _ySelector(0))
-                .attr('x2', drawWidth)
-                .attr('y2', _ySelector(0))
+    // Renders a static legend in the left hand corner of
+    // the chart
+    function renderLegend() {
+        var padding = 20;
+        var legend = svg.append('g')
+                .attr('class', 'legend')
+                .attr('transform', 'translate(' + (chartMargin.left + padding) + 
+                    ',' + (chartMargin.top + padding) + ')');
+        
+        legend.append('rect')
+                .attr('x', 0)
+                .attr('y', 0)
+                .attr('width', 110)
+                .attr('height', 50)
+                .attr('fill', 'white')
                 .attr('stroke-width', 1)
                 .attr('stroke', 'rgba(15, 15, 15, 0.61)');
+        
+        var row = legend.append('g')
+                .attr('transform', 'translate(' + (padding/2) + 
+                    ',' + (padding/2) + ')');
+        
+        row.append('rect')
+                .attr('x', 0)
+                .attr('y', 0)
+                .attr('width', 10)
+                .attr('height', 10)
+                .attr('fill', '#1ae9f0')
+                .attr('stroke-width', 1)
+                .attr('stroke', 'rgba(15, 15, 15, 0.61)');
+        
+        row.append('text')
+                .attr('x', 15)
+                .attr('y', 10)
+                .attr('font-size', '12px')
+                .text('Negative Mean');
+
+        var row2 = legend.append('g')
+                .attr('transform', 'translate(' + (padding/2) + 
+                    ',' + (padding + 10) + ')');
+        
+        row2.append('rect')
+                .attr('x', 0)
+                .attr('y', 0)
+                .attr('width', 10)
+                .attr('height', 10)
+                .attr('fill', '#662112')
+                .attr('stroke-width', 1)
+                .attr('stroke', 'rgba(15, 15, 15, 0.61)');
+        
+        row2.append('text')
+                .attr('x', 15)
+                .attr('y', 10)
+                .attr('font-size', '12px')
+                .text('Positive Mean');
     }
 
     // Handle the interactive selection and panning
     function setupBrush() {
         _brush = d3.brushX()
             .extent([[0, 0], [drawWidth, selectorHeight]])
-            .on("brush end", brushed);
+            .on('brush end', brushed);
     }
 
     function brushed() {
-        if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
+        if (d3.event.sourceEvent && d3.event.sourceEvent.type === 'zoom') return; // ignore brush-by-zoom
     
         var s = d3.event.selection || _xSelector.range();
         _xChart.domain(s.map(_xSelector.invert, _xSelector));
         renderBars(_chart, _byYear, _xChart, _yChart, chartHeight, true);
 
-        _chart.select(".axis--x").call(_xChartAxis);
-        svg.select(".zoom").call(_zoom.transform, d3.zoomIdentity
+        _chart.select('.axis--x').call(_xChartAxis);
+        svg.select('.zoom').call(_zoom.transform, d3.zoomIdentity
             .scale(drawWidth / (s[1] - s[0]))
             .translate(-s[0], 0));
     }
@@ -218,27 +284,27 @@ function init() {
             .scaleExtent([1, Infinity])
             .translateExtent([[0, 0], [drawWidth, chartHeight]])
             .extent([[0, 0], [drawWidth, chartHeight]])
-            .on("zoom", zoomed);
+            .on('zoom', zoomed);
         
-        svg.append("rect")
-            .attr("class", "zoom")
-            .attr("width", drawWidth)
-            .attr("height", chartHeight)
-            .attr("transform", "translate(" + chartMargin.left + "," + chartMargin.top + ")")
+        svg.append('rect')
+            .attr('class', 'zoom')
+            .attr('width', drawWidth)
+            .attr('height', chartHeight)
+            .attr('transform', 'translate(' + chartMargin.left + ',' + chartMargin.top + ')')
             .call(_zoom);
     }
 
     function zoomed() {
-        if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
+        if (d3.event.sourceEvent && d3.event.sourceEvent.type === 'brush') return; // ignore zoom-by-brush
         var t = d3.event.transform;
         _xChart.domain(t.rescaleX(_xSelector).domain());
         renderBars(_chart, _byYear, _xChart, _yChart, chartHeight, true);
-        _chart.select(".axis--x").call(_xChartAxis);
-        _selector.select(".brush").call(_brush.move, _xChart.range().map(t.invertX, t));
+        _chart.select('.axis--x').call(_xChartAxis);
+        _selector.select('.brush').call(_brush.move, _xChart.range().map(t.invertX, t));
     }
 
     // Functions to handle the data cleaning and processing
-    var parseDate = d3.timeParse("%b %Y");
+    var parseDate = d3.timeParse('%b %Y');
 
     // Clean and Filter the raw data read from the CSV
     // into a usable form
